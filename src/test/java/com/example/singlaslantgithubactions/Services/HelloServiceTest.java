45491a6b9fca98ffcfc5aedbd76b6835421236b4
Maria Hagelin <maria.hagelin@yh.nackademin.se>
2023-10-04 10:51:56 +0200

    separate tests and docker needs both tests to go green

---
+++ src/test/java/com/example/singlaslantgithubactions/Services/HelloServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.singlaslantgithubactions.Services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.example.singlaslantgithubactions.Model.CoinFlip;
+import com.example.singlaslantgithubactions.Model.RoundResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.junit.jupiter.api.Assertions.*;
+public class HelloServiceTest {
+
+    private HelloService helloService;
+    @BeforeEach
+    void setHelloService(){
+        HelloService helloService = new HelloService();
+    }
+    @Test
+    void returnHelloReturnsHello(){
+        assertEquals(helloService.returnHello(), "GOODBYE!!!");
+    }
+}
